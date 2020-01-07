e66fdfbbb8976f804bcdded0bd9a3c5815a3d25f
Klaudia Kriva <klaudia.kriva@akademiasovy.sk>
2020-01-07 21:43:59 +0100

    added dropdowns etc

--- EditPublications/src/app/form/form.component.ts
+++ EditPublications/src/app/form/form.component.ts
@@ -15,6 +15,21 @@ export interface Activity{
   viewValue: string;
 }
 
+export interface ResearchField{
+  value: string;
+  viewValue: string;
+}
+
+export interface Type{
+  value: string,
+  viewValue: string
+}
+
+export interface GantScheme{
+  value:string,
+  viewValue:string
+}
+
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
@@ -28,6 +43,63 @@ export class FormComponent implements OnInit {
     {value: 'yesE-2', viewValue: 'Ano - externy'}
   ];
 
+  gantSchemes: GantScheme[] = [
+    {value: "neurcena", viewValue: 'Vyberte grantovú schému'},
+    {value: "apvv", viewValue: 'APVV'},
+    {value: "vega", viewValue: 'VEGA'},
+    {value: "kega", viewValue: 'KEGA'},
+    {value: "vvgs_upjs", viewValue: 'VVGS UPJŠ'},
+    {value: "vvgs_pf_upjs", viewValue: 'VVGS PF UPJŠ'},
+    {value: "vvgs_lf_upjs", viewValue: 'VVGS LF UPJŠ'},
+    {value: "vvgs_ff_upjs", viewValue: 'VVGS FF UPJŠ'},
+    {value: "vvgs_pravf_upjs", viewValue: 'VVGS PrávF UPJŠ'},
+    {value: "vvgs_fvs_upjs", viewValue: 'VVGS FVS UPJŠ'},
+    {value: "cern", viewValue: 'CERN'},
+    {value: "cost", viewValue: 'COST<'},
+    {value: "visegrad", viewValue: 'Visegrad fund'},
+    {value: "opvai", viewValue: 'OP VaI'},
+    {value: "interreg", viewValue: 'INTERREG'},
+    {value: "horizont", viewValue: 'Horizont 2020'},
+    {value: "msvvas", viewValue: 'MŠVVaŠ SR'},
+    {value: "ina", viewValue: 'Iná - upresniť'}
+  ]
+
+  types: Type[]= [
+    {value: "neurceny", viewValue: 'Vyberte typ dokumentu'},
+      {value: "monografia_zbornik", viewValue: 'Monografia/Zborní'},
+      {value: "clanok_kapitola", viewValue: 'Článok v zborníku/kapitola v knih'},
+      {value: "clanok_casopis", viewValue: 'Článok v časopis'},
+  ]
+
+  researchFields: ResearchField[]= [
+    {value: "neurceny", viewValue: 'Vyberte oblasť výskumu'},
+      {value: "010", viewValue: 'Pedagogické vedy'},
+      {value: "020", viewValue: 'Humanitné vedy'},
+      {value: "030", viewValue: 'Historické vedy a etnológia'},
+      {value: "040", viewValue: 'Umenie'},
+      {value: "050", viewValue: 'Projektovanie, inžinierstvo, technológie a vodné hospodárstvo'},
+      {value: "060", viewValue: 'Spoločenské a behaviorálne vedy'},
+      {value: "070", viewValue: 'Právo a medzinárodné vzťahy'},
+      {value: "080", viewValue: 'Ekonómia a manažment'},
+      {value: "091", viewValue: 'Fyzika'},
+      {value: "092", viewValue: 'Vedy o Zemi a vesmíre'},
+      {value: "100", viewValue: 'Environmentalistika a ekológia'},
+      {value: "110", viewValue: 'Metalurgické a montánne vedy'},
+      {value: "120", viewValue: 'Chémia, chemická technológia a biotechnológie'},
+      {value: "130", viewValue: 'Vedy o živej prírode'},
+      {value: "140", viewValue: 'Strojárstvo'},
+      {value: "150", viewValue: 'Elektrotechnika a elektroenergetika'},
+      {value: "160", viewValue: 'Informatické vedy, automatizácia a telekomunikácie'},
+      {value: "170", viewValue: 'Inžinierstvo a technológie'},
+      {value: "180", viewValue: 'Lekárske, farmaceutické a nelekárske zdravotnícke vedy'},
+      {value: "190", viewValue: 'Poľnohospodárske a lesnícke vedy'},
+      {value: "200", viewValue: 'Veterinárske vedy'},
+      {value: "210", viewValue: 'Vedy o športe'},
+      {value: "220", viewValue: 'Dopravné služby'},
+      {value: "230", viewValue: 'Bezpečnostné služby'},
+      {value: "240", viewValue: 'Matematika a štatistika'}
+  ]
+
   workspaces: Workspace[]= [
     {value: '0', viewValue: 'Vyberte pracovisko'},
     {value: '1', viewValue: 'Prirodovedecka fakulta'},
@@ -38,6 +110,8 @@ export class FormComponent implements OnInit {
     {value: '6', viewValue: 'Univerzitne pracovisko'},
     {value: '7', viewValue: 'Ine - nie z UPJS'},
   ];
+
+
 
   activities: Activity[]= [
     {value: "neurceny", viewValue: 'Vyberte kategóriu publikačnej činnosti'},
